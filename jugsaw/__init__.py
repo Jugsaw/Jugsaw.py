0d1cc01f0949c2b4f492bbc6251340a90435c86f
GiggleLiu <cacate0129@gmail.com>
2023-10-08 10:06:48 +0800

    export a new interface

--- jugsaw/__init__.py
+++ jugsaw/__init__.py
@@ -1,3 +1,3 @@
 from .app import App, DemoRef, Demo, request_app
-from .remotecall import call, fetch, healthz, ClientContext, jsoncall
+from .remotecall import call, fetch, healthz, ClientContext, jsoncall, new_request_demos
 from .simpleparser import JDataType
